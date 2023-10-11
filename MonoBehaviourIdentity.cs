debcf15c755f08e59fc3c2eff3ff6240177640ac
Maxim <36982547+Siphoin@users.noreply.github.com>
2023-10-11 12:02:45 +0500

    Add files via upload

---
+++ MonoBehaviourIdentity.cs
@@ -0,0 +1,65 @@
+﻿using AboveSnakes.Attributes;
+using RandomizableFields;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace AboveSnakes
+{
+    public class MonoBehaviourIdentity : MonoBehaviour
+    {
+         [SerializeField, ReadOnly(ReadOnlyMode.OnPlayMode)] private string _guid;
+
+        public string GUID => _guid;
+
+        public void SetGuid()
+        {
+            _guid = Guid.NewGuid().ToString();
+        }
+
+        public void SetGuid(string guid)
+        {
+            if (!Guid.TryParse(guid, out Guid _))
+            {
+                return;
+            }
+
+            _guid = guid;
+        }
+
+#if UNITY_EDITOR
+
+        private void OnValidate()
+        {
+
+            ValidateGUID();
+        }
+
+        public void ValidateGUID()
+        {
+
+#pragma warning disable CS0618 // Тип или член устарел
+            PrefabType prefabType = PrefabUtility.GetPrefabType(gameObject);
+#pragma warning restore CS0618 // Тип или член устарел
+
+            if (Application.isPlaying)
+            {
+                return;
+            }
+
+            if (prefabType == PrefabType.PrefabInstance && string.IsNullOrEmpty(_guid))
+            {
+                SetGuid();
+            }
+
+            else if (prefabType != PrefabType.PrefabInstance)
+            {
+                _guid = string.Empty;
+            }
+        }
+
+#endif
+    }
+}
---
+++ ScriptableObjectIdentity.cs
@@ -0,0 +1,14 @@
+﻿using AboveSnakes.Interfaces;
+using RandomizableFields;
+using System;
+using UnityEngine;
+
+namespace AboveSnakes.GameDesign
+{
+    public class ScriptableObjectIdentity : ScriptableObject, Iidentity
+    {
+        [SerializeField, RandomizableGuid] private string _guid = Guid.NewGuid().ToString();
+
+        public string GUID => _guid;
+    }
+}
