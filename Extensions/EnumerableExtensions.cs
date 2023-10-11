2c63335cf53ef21c3ddb3b3d66c6d5ac9ba24b5a
Maxim <36982547+Siphoin@users.noreply.github.com>
2023-10-11 13:49:41 +0500

    Create EnumerableExtensions.cs

---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SiphoinUnityHelpers.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection)
+        {
+            Random random = new Random();
+
+            return collection.OrderBy(x => random.Next());
+        }
+    }
+}
