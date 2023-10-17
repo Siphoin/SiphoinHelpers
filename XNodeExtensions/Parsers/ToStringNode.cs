7ced6cde08e43c47731474e8a49fec5905a5f54b
Maxim <36982547+Siphoin@users.noreply.github.com>
2023-10-17 17:19:39 +0500

    xNode extensions V8

--- XNodeExtensions/Parsers/ToStringNode.cs
+++ XNodeExtensions/Parsers/ToStringNode.cs
@@ -3,15 +3,18 @@ using XNode;
 
 namespace SiphoinUnityHelpers.XNodeExtensions.Parsers
 {
-    public class ToStringNode : ParseNode<Object, string>
+    public class ToStringNode : BaseNode
     {
+        [Input(ShowBackingValue.Never, ConnectionType.Override), SerializeField] private NodePort _input;
+
+        [Output(ShowBackingValue.Never), SerializeField] private string _output;
         public override object GetValue(NodePort port)
         {
             if (!Application.isPlaying)
             {
                 return base.GetValue(port);
             }
-          return GetInputObject().ToString();
+             return GetInputPort(nameof(_input)).Connection.GetOutputValue().ToString();
         }
     }
 }
