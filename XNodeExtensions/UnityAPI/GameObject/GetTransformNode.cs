58b729221af919119cbf0ba5b52548d54a13f3f0
Maxim <36982547+Siphoin@users.noreply.github.com>
2023-10-18 15:10:55 +0500

    xNode extensions V10

---
+++ XNodeExtensions/UnityAPI/GameObject/GetTransformNode.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using XNode;
+
+namespace SiphoinUnityHelpers.XNodeExtensions.UnityAPI.GameObjects
+{
+    public class GetTransformNode : BaseNode
+    {
+        [Input(ShowBackingValue.Never, ConnectionType.Override), SerializeField] private GameObject _gameObject;
+
+        [Output(ShowBackingValue.Never), SerializeField] private Transform _output;
+
+        public override object GetValue(NodePort port)
+        {
+            if (!Application.isPlaying)
+            {
+                return base.GetValue(port);
+            }
+
+            var gameObject = GetDataFromPort<GameObject>(nameof(_gameObject));
+
+            return gameObject.transform;
+        }
+    }
+}
